7179b3eea9a02e072898b7e20c49864342c52452
Kate Sickora <katelindsay32@gmail.com>
2020-05-11 22:47:28 -0500

    refactor

--- src/test/java/MazeBuilderTest.java
+++ src/test/java/MazeBuilderTest.java
@@ -1,15 +1,10 @@
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mockito;
-
 import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Scanner;
-
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 public class MazeBuilderTest {
 
@@ -25,9 +20,7 @@ public class MazeBuilderTest {
     @Test
     public void buildMazeFromDimensions() {
 
-        ArrayList<String> mazeArguments = new ArrayList<String>(Arrays.asList(
-                "1,1",
-                "-1"));
+        ArrayList<String> mazeArguments = createArguments("1,1");
         Room[][] maze = mazeBuilder.build(mazeArguments);
         assertEquals(1, maze.length);
         assertEquals(1, maze[0].length);
@@ -35,34 +28,32 @@ public class MazeBuilderTest {
 
     @Test(expected = IllegalArgumentException.class)
     public void validateDimensionsAreNumbers() {
-        ArrayList<String> mazeArguments = new ArrayList<String>(Arrays.asList(
-                "-asdf",
-                "-1"));
+        ArrayList<String> mazeArguments = createArguments("-asdf");
         Room[][] maze = mazeBuilder.build(mazeArguments);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void validateThereAreTwoDimensions() {
-        ArrayList<String> mazeArguments = new ArrayList<String>(Arrays.asList(
-                "-3,3,3",
-                "-1"));
+        ArrayList<String> mazeArguments = createArguments("-3,3,3");
         Room[][] maze = mazeBuilder.build(mazeArguments);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void validatexDimensionGreaterThanZero() {
-        ArrayList<String> mazeArguments = new ArrayList<String>(Arrays.asList(
-                "1,0",
-                "-1"));
+        ArrayList<String> mazeArguments = createArguments("1,0");
         Room[][] maze = mazeBuilder.build(mazeArguments);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void validateyDimensionIsGreaterThanZero() {
-        ArrayList<String> mazeArguments = new ArrayList<String>(Arrays.asList(
-                "0,1",
-                "-1"));
+        ArrayList<String> mazeArguments = createArguments("0,1");
         Room[][] maze = mazeBuilder.build(mazeArguments);
+    }
+
+    private ArrayList<String> createArguments(String dimensions) {
+        return new ArrayList<String>(Arrays.asList(
+                dimensions,
+                "-1"));
     }
 
 }
